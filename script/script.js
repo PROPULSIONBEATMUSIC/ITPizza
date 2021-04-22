95f4624d46aabd3dc54659c8b61e6ba5dcab40bb
Kirill <narushev.k@mail.ru>
2021-04-22 16:09:25 +0300

    commit1

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let basket = {
+    'pizza_for_JS': 2,
+    'pizza_for_PY': 1,
+    'pizza_for_C': 4,
+    'pizza_for_BF': 3,
+};
+
+document.onclick = event => {
+    console.log(event.target.classList);
+}
